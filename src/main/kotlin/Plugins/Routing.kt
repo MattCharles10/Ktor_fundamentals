cc7edb7051a5294cd49ec7bbd1a8da4e9213e2ed
Mathew <matthewcharles.tech@gmail.com>
2026-02-18 12:10:59 +0530

    File Upload done

--- src/main/kotlin/Plugins/Routing.kt
+++ src/main/kotlin/Plugins/Routing.kt
@@ -1,12 +1,14 @@
 package com.mathew.Plugins
 
-import io.ktor.http.HttpMethod
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.resources.Resource
+import io.ktor.server.request.receive
+import io.ktor.server.request.receiveChannel
 import io.ktor.server.request.receiveText
-import io.ktor.server.resources.*
+import io.ktor.utils.io.readRemaining
+import io.ktor.utils.io.readText
+import java.io.File
 
 fun Application.configureRouting() {
 
@@ -19,11 +21,32 @@ fun Application.configureRouting() {
             val name = call.receiveText()
             call.respondText { "Hello , $name" }
         }
+
+        post("channel") {
+            val channel = call.receiveChannel()
+            val test = channel.readRemaining().readText()
+            call.respondText(test)
+        }
+
+        //file uploading
+        post("upload"){
+
+            val file = File("uploads/sample.jpg").apply {
+                parentFile?.mkdirs()
+            }
+
+            val byteArray = call.receive<ByteArray>()
+
+            file.writeBytes(byteArray)
+
+            call.respondText("File upload success")
+
+        }
     }
 
 }
 
-/*    */
+/*          */
 
 
 
