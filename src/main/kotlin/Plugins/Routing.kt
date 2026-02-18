185ce9fc9db1084f5fcf5bd940a615d9b4bb5601
Mathew <matthewcharles.tech@gmail.com>
2026-02-18 11:46:53 +0530

    Add Ktor /greet POST endpoint and homepage - 2026-02-18

--- src/main/kotlin/Plugins/Routing.kt
+++ src/main/kotlin/Plugins/Routing.kt
@@ -5,81 +5,25 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.resources.Resource
+import io.ktor.server.request.receiveText
 import io.ktor.server.resources.*
 
 fun Application.configureRouting() {
 
-    install(RoutingRoot){
-        route("/" , HttpMethod.Get){
-            handle {
-                call.respondText { "Hello world 123" }
-            }
-        }
-    }
-
     routing {
         get("/") {
-            call.respondText("Hello World!")
-        }
-        get("blogs/{id}"){
-            val id = call.pathParameters["id"]
-            val q1 = call.queryParameters["q1"]
-            val q2 = call.queryParameters["q2"]
-            call.respondText { "Blog with id $id and query is $q1 & query 2 is $q2"  }
+            call.respondText("Server is running!")
         }
 
-        get(Regex(".+/test")){
-            call.respondText { "Api_response_test" }
-        }
-
-        //api/v1/users
-        //api/v2/users
-        //api/v3/users
-
-        get(Regex("api/(?<apiVersion>v[1-3])/users")){
-            val version = call.pathParameters["apiVersion"]
-            call.respondText { "Api Version is $version" }
-        }
-
-        get<Blogs> { blogs ->
-            val sort = blogs.sort
-            call.respondText("Blogs sorted by $sort")
-        }
-        delete<Blogs.Blog> { blogs ->
-            val sort = blogs.parent.sort
-            val id = blogs.id
-            call.respondText { "Blog id : $id sorting is based on $sort" }
-
-        }
-
-        //nested routes - to make much more readable
-        accountRoutes()
-
-    }
-}
-
-fun Route.accountRoutes(){
-    route("accounts"){
-        route("users"){
-            get{ }
-            get("{id}"){}
-            post(""){}
-            patch("{id}"){}
-        }
-        route("auth"){
-            post("login"){ }
-            post("signup"){ }
+        post("greet"){
+            val name = call.receiveText()
+            call.respondText { "Hello , $name" }
         }
     }
+
 }
 
 /*    */
-
-@Resource("blogs")
-    class Blogs(val sort : String? = "new"){
-        @Resource("{id}")
-        data class Blog(val parent:Blogs = Blogs(),val id:String)
-    }
 
 
 
